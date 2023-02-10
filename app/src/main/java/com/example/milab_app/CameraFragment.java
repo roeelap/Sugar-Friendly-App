aa4d92f4bed591e02d9ac25ce0512ecc035588cb
roee lapushin <rlapushin@gmail.com>
2023-02-10 22:43:35 +0200

    fixed camera - now using just plain intent to camera app on phone

--- app/src/main/java/com/example/milab_app/CameraFragment.java
+++ app/src/main/java/com/example/milab_app/CameraFragment.java
@@ -1,43 +1,94 @@
 package com.example.milab_app;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.hardware.camera2.CameraCaptureSession;
-import android.hardware.camera2.CameraDevice;
-import android.hardware.camera2.CaptureRequest;
-import android.media.ImageReader;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 
-import android.os.Handler;
-import android.os.HandlerThread;
-import android.util.Size;
-import android.util.SparseIntArray;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
 import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class CameraFragment extends Fragment {
 
+    private static final String TAG = "CameraFragment";
+
+    ActivityResultLauncher<Intent> activityResultLauncher;
+    private String currentPhotoPath;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_camera, container, false);
 
-        Button uploadImageButton = rootView.findViewById(R.id.upload_image_button);
-        uploadImageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getActivity(), CameraActivity.class);
-                startActivity(intent);
-            }
-        });
+        activityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    Log.e(TAG, "Got image: " + currentPhotoPath);
+                    displayImage();
+                });
 
+        Button uploadImageButton = rootView.findViewById(R.id.upload_image_button);
+        uploadImageButton.setOnClickListener(this::captureImage);
 
         return rootView;
+    }
+
+
+    public void captureImage(View view) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(requireActivity().getPackageManager()) != null) {
+            File imageFile = null;
+            try {
+                imageFile = createImageFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            if (imageFile != null) {
+                Uri imageUri = FileProvider.getUriForFile(requireContext(),
+                        "com.example.android.fileprovider", imageFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                activityResultLauncher.launch(takePictureIntent);
+            }
+        }
+    }
+
+    public void displayImage() {
+        Intent intent = new Intent(requireContext(), DisplayImageActivity.class);
+        intent.putExtra("imagePath", currentPhotoPath);
+        startActivity(intent);
+    }
+
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        @SuppressLint("SimpleDateFormat")
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = requireActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
     }
 }
---
+++ app/src/main/java/com/example/milab_app/DisplayImageActivity.java
@@ -0,0 +1,57 @@
+package com.example.milab_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import androidx.exifinterface.media.ExifInterface;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import java.io.IOException;
+
+public class DisplayImageActivity extends AppCompatActivity {
+
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_image);
+
+        imageView = findViewById(R.id.display_image_view);
+        String photoPath = getIntent().getStringExtra("imagePath");
+        Bitmap bitmap = BitmapFactory.decodeFile(photoPath);
+        bitmap = rotateImageIfNeeded(photoPath, bitmap);
+        imageView.setImageBitmap(bitmap);
+    }
+
+    private static Bitmap rotateImageIfNeeded(String photoPath, Bitmap img) {
+        ExifInterface ei;
+        try {
+            ei = new ExifInterface(photoPath);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        int orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+        switch (orientation) {
+            case ExifInterface.ORIENTATION_ROTATE_90:
+                return rotateImage(img, 90);
+            case ExifInterface.ORIENTATION_ROTATE_180:
+                return rotateImage(img, 180);
+            case ExifInterface.ORIENTATION_ROTATE_270:
+                return rotateImage(img, 270);
+            default:
+                return img;
+        }
+    }
+
+    private static Bitmap rotateImage(Bitmap img, int degree) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(degree);
+        Bitmap rotatedImg = Bitmap.createBitmap(img, 0, 0, img.getWidth(), img.getHeight(), matrix, true);
+        img.recycle();
+        return rotatedImg;
+    }
+}
